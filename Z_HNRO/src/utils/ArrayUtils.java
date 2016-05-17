a5beb8d4747210e2354e1e2120d4f647228dea07
keiaz <keiaz@DESKTOP-3DP8JQU>
2016-05-17 10:48:04 +0900

    ddd

--- Z_HNRO/src/utils/ArrayUtils.java
+++ Z_HNRO/src/utils/ArrayUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayUtils {
 	public static void main(String[] args) {
@@ -10,13 +13,26 @@ public class ArrayUtils {
 		arr.add("hello~");
 		arr.add("12345");
 		
-		String[] result = toArray(arr);
-		System.out.println(result);
+		System.out.println(subList(arr, 1, arr.size()).toString());
 	}
 	
 	public static String join(ArrayList<String> arr) {
 		String result = arr.toString();
 		result = result.substring(1, result.length() - 1);
+		return result;
+	}
+	
+	/**
+	 * [0,1,2,3]을 1,3으로 끊으면 [1,2]가 반환된다
+	 * @param arr
+	 * @param startIndex
+	 * @param endIndex
+	 * @return
+	 */
+	public static ArrayList<String> subList(ArrayList<String> arr, int startIndex, int endIndex) {
+		List<String> subResult = arr.subList(startIndex, endIndex);
+		ArrayList<String> result = new ArrayList<>();
+		result.addAll(subResult);
 		return result;
 	}
 	
--- Z_HNRO/src/utils/NumberUtils.java
+++ Z_HNRO/src/utils/NumberUtils.java
@@ -1,10 +1,36 @@
 package utils;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class NumberUtils {
 	public static void main(String[] args) {
-		System.out.println(toFixed(3.1, 3));
+		System.out.println(devide(10, 0));
 	}
 	
+	/**
+	 * 나누기
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public static double devide(double num1, double num2) {
+		if (num2 == 0) {
+			return 0;
+		}
+		
+		BigDecimal db1 = BigDecimal.valueOf(num1);
+		BigDecimal db2 = BigDecimal.valueOf(num2);
+		
+		return db1.divide(db2, MathContext.DECIMAL128).doubleValue();
+	}
+	
+	/**
+	 * 소수점 자리수 고정
+	 * @param num
+	 * @param digit
+	 * @return
+	 */
 	public static String toFixed(double num, int digit) {
 		String snum = Double.toString(num);
 		int dotIdx = snum.indexOf(".");
